9c0addb0fe152ca5db639c8056bdcd5b325b03a0
sayuroy <balotje918@gmail.com>
2020-05-14 16:43:56 +0900

    deproy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.0"
+lock "3.14.0"
 
 set :application, "Portfolio"
 set :repo_url, "git@github.com:sayuroy/Portfolio.git"
