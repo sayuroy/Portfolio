b5d6214e401c4c1078b7a781b426193bf899a6a4
sayuroy <balotje918@gmail.com>
2020-05-15 12:22:27 +0900

    total view image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tops#index"
+  resources :tops, only: :index
+  get 'tops/:name', controller: 'tops', action: 'index'
 end
