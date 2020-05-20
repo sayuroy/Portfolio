334272850659676f210a7ec1206cb753b1a527d4
sayuroy <balotje918@gmail.com>
2020-05-20 22:28:12 +0900

    view style change

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,22 @@
 class TopsController < ApplicationController
   def index
-    
+  end
+
+  def create
+    message = Model.new(message_params)
+    if message.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+
+
+  private
+
+  def message_params
+    params.permit(:name, :title, :message)
   end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,6 @@
 class Model < ApplicationRecord
+
+  validates :name, presence: true, length: { in: 1..40 }
+  validates :title, presence: true, length: { in: 1..100 }
+  validates :message, presence: true, length: { in: 1..2000 }
 end
