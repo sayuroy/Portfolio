d53f8d710ad867e1841a43ee1bf709626d6408c6
sayuroy <balotje918@gmail.com>
2020-05-20 22:28:28 +0900

    js update

---
+++ app/assets/javascripts/tops.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".btn").click(function() {
+    var target = $(this).attr('id') + 'top';
+    var element = document.getElementById(target);
+    var rect = element.getBoundingClientRect();
+    var position = rect.top + window.pageYOffset;
+    $("html, body").animate({scrollTop:position}, 400, 'swing');
+    return false;
+  });
+});
