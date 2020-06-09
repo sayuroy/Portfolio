d64700a4d95b1e996d09a37fdf9459c5c89e6dca
sayuroy <balotje918@gmail.com>
2020-06-09 21:59:57 +0900

    memo create

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -8,10 +8,23 @@ class MemosController < ApplicationController
     @memo = Memo.new
   end
 
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to memos_path
+    else
+      render :new
+    end
+  end
+
 
   private
 
   def signin_check
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def memo_params
+    params.require(:memo).permit(:title, :text, :public, :price).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     resources :tops, only: :index, defaults: { format: 'json' }
   end
 
-  resources :memos, only: [:index, :new]
+  resources :memos, only: [:index, :new, :create]
 end
